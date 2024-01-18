41832fea99e9723baaf9a21b9adbdac12d13bbb9
Bedknobs <noeliareysilva@gmail.com>
2024-01-18 09:14:30 +0100

    improved switch method

--- src/components/Simbolos.jsx
+++ src/components/Simbolos.jsx
@@ -5,23 +5,17 @@ export const simbolo = (description) => {
     case "Nubes altas":
       return <img src="../../src/assets/wi-cloudy.svg" alt="nubes" />;
     case "Poco nuboso":
-      return <img src="../../src/assets/wi-day-cloudy.svg" alt="nubes y sol" />;
     case "Intervalos nubosos":
       return <img src="../../src/assets/wi-day-cloudy.svg" alt="nubes y sol" />;
     case "Cubierto con lluvia":
-      return <img src="../../src/assets/wi-rain.svg" alt="lluvia" />;
-    case "Cubierto con lluvia escasa":
-      return <img src="../../src/assets/wi-rain-mix.svg" alt="lluvia escasa" />;
-    case "Muy nuboso con lluvia escasa":
-      return <img src="../../src/assets/wi-rain-mix.svg" alt="lluvia escasa" />;
     case "Nuboso con lluvia":
-      return <img src="../../src/assets/wi-rain.svg" alt="lluvia" />;
-    case "Intervalos nubosos con lluvia escasa":
-      return <img src="../../src/assets/wi-rain-mix.svg" alt="lluvia escasa" />;
     case "Muy nuboso con lluvia":
-      return <img src="../../src/assets/wi-rain.svg" alt="lluvia" />;
     case "Intervalos nubosos con lluvia":
       return <img src="../../src/assets/wi-rain.svg" alt="lluvia" />;
+    case "Cubierto con lluvia escasa":
+    case "Muy nuboso con lluvia escasa":
+    case "Intervalos nubosos con lluvia escasa":
+      return <img src="../../src/assets/wi-rain-mix.svg" alt="lluvia escasa" />;
     default:
       return "Símbolo predeterminado";
   }
